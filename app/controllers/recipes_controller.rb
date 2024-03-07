71f86a8ff281040b6f43ee8a259e9f123f9c3fcb
Samuel Demolombe <samuel.dmlb@gmail.com>
2024-03-07 12:30:18 +0000

    ajout des ingredients et des steps a la show de recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,5 +6,7 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
     @user_recipe = UserRecipe.new
+    @recipe_ingredients = RecipeIngredient.where(recipe_id: @recipe.id)
+    @steps = Step.where(recipe_id: @recipe.id)
   end
 end
