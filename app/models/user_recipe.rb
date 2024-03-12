73ad392a76867f70ddff56b19db4c0df24776476
Samuel Demolombe <samuel.dmlb@gmail.com>
2024-03-12 22:13:21 +0000

    logique des keywords

--- app/models/user_recipe.rb
+++ app/models/user_recipe.rb
@@ -11,6 +11,7 @@ class UserRecipe < ApplicationRecord
   def check_and_create_user_badge
     badge_country3
     badge_difficulty3
+    badge_keyword3
   end
 
   def badge_country3
@@ -19,13 +20,11 @@ class UserRecipe < ApplicationRecord
     # Nombre de user_recipe ayant la meme condition
     user_recipes_count = UserRecipe.where(user_id:).joins(recipe: :country).where(countries: { id: country_id }).count
 
-    # Correspondance entre le pays et le badge
-    badge_name = case Country.find(country_id).name
-                 when "france"
-                   "Badge France"
-                 when "italy"
-                   "Badge Italy"
-                 end
+    # Correspondance entre la condition et le badge
+    country_hash = { france: "Badge France",
+                     italy: "Badge Italy" }
+
+    badge_name = country_hash[Country.find(country_id).name.to_sym]
 
     # Créer un user_badge avec le badge correspondant si la condition est remplise
     UserBadge.create(user_id:, badge: Badge.find_by(name: badge_name)) if user_recipes_count == 3
@@ -35,19 +34,39 @@ class UserRecipe < ApplicationRecord
     difficulty = recipe.difficulty
 
     # Nombre de user_recipe ayant la meme condition
-    user_recipes_count = UserRecipe.joins(:recipe).where(recipes: { difficulty: }).count
+    user_recipes_count = UserRecipe.where(user_id:).joins(:recipe).where(recipes: { difficulty: }).count
 
-    # Correspondance entre la difficulté et le badge
-    badge_name = case difficulty
-                 when "easy"
-                   "Badge Easy"
-                 when "medium"
-                   "Badge Medium"
-                 when "hard"
-                   "Badge Hard"
-                 end
+    # Correspondance entre la condition et le badge
+    difficulty_hash = { easy: "Badge Easy",
+                        medium: "Badge Medium",
+                        hard: "Badge Hard" }
+
+    badge_name = difficulty_hash[difficulty.to_sym]
 
     # Créer un user_badge avec le badge correspondant si la condition est remplise
     UserBadge.create(user_id:, badge: Badge.find_by(name: badge_name)) if user_recipes_count == 3
+  end
+
+  def badge_keyword3
+    keywords = recipe.keyword_list
+
+    keywords.each do |keyword|
+      # Nombre de user_recipe ayant la meme condition
+      user_recipes_count = UserRecipe.where(user_id:).joins(recipe: :keywords).where(keywords: { name: keyword }).count
+
+      # Correspondance entre la condition et le badge
+      keywords_hash = { pork: "Badge Pork",
+                        beef: "Badge Beef",
+                        chicken: "Badge Chicken",
+                        vegan: "Badge Vegan",
+                        vegetarian: "Badge Vegetarian",
+                        dessert: "Badge Dessert",
+                        duck: "Badge Duck" }
+
+      badge_name = keywords_hash[keyword.to_sym]
+
+      # Créer un user_badge avec le badge correspondant si la condition est remplise
+      UserBadge.create(user_id:, badge: Badge.find_by(name: badge_name)) if user_recipes_count == 3
+    end
   end
 end
