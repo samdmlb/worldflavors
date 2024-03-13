6b1890865f4cf1cbcc29f27077f848b31c90908f
Erwan RAHIM <waren78.krahim@gmail.com>
2024-03-13 17:11:47 +0100

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get 'dashboard', to: 'pages#dashboard'
   resources :recipes, only: [:index, :show,] do
     resources :user_recipes, only: [:create]
   end
