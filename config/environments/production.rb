1a8083c8fa269abe663a2f01f2ec9cff30427b57
Samuel Demolombe <samuel.dmlb@gmail.com>
2024-03-26 22:15:49 +0000

    test prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.serve_static_assets = true # ajouté le 26/03
+  config.assets.compile = true # était sur false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
